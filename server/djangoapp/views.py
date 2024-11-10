ad717a7684444ae4572f97b42eba6c0e818f055e
alstondsouza <49649815+alstondsouza@users.noreply.github.com>
2024-11-11 00:12:43 +0530

    code linting

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -92,7 +92,7 @@ def registration(request):
 # a list of dealerships
 # def get_dealerships(request):
 # ...
-# Update the `get_dealerships` render list of dealerships 
+# Update the `get_dealerships` render list of dealerships
 # all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
@@ -160,5 +160,7 @@ def get_cars(request):
     car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name})
     return JsonResponse({"CarModels": cars})
